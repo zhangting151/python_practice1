f43ab7ab8be6e6520e57a4fa910ca4ff02b25907
zhangting151@163.com <i151186>
2014-02-08 09:22:48 +0800

    ex1.py
    ex1.py

---
+++ python/ex1.py
@@ -0,0 +1,11 @@
+#coding: utf-8
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay!Printing.'
+print "I'd much rather you 'not'"
+print 'I "said" do not touch  this.'
+
+#单引号与双引号作用一样
+#井号代表注释
