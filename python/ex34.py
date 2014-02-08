1a710ca5dd4965b0fcb16a672ea7c473d87a73d8
zhangting151@163.com <i151186>
2014-02-08 09:51:09 +0800

    ex34.py

---
+++ python/ex34.py
@@ -0,0 +1,11 @@
+animals = ['bear','python','peacock','kangaroo','whale','platpus']
+
+print "the animal at 1: ",animals[1]
+print "The 3rd animal: ",animals[2]
+print "The 1st annimal: ",animals[0]
+print "the annimal at 3: ",animals[3]
+print "The 5th animal: ",animals[4]
+print "the annimal at 2: ",animals[2]
+print "The 6th animal: ",animals[5]
+print "the annimal at 4: ",animals[4]
+
