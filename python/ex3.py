e2a877c720c5e63d5f4599331a64a341e1106e6a
zhangting151@163.com <i151186>
2014-02-08 09:39:37 +0800

    ex3.py
    ex3.py

---
+++ python/ex3.py
@@ -0,0 +1,38 @@
+#coding:utf-8
+print "I will now count my chickems:"
+
+#/代表除法，整数除法结果取整，浮点数除法结果是浮点数
+#除法练习
+print "6/5=",6/5
+print "6.0/5=",6.0/5
+print "1/3=",1/3
+print "1/3.0=",1/3.0
+
+print "Hens",25 + 30 / 6
+
+#%为取余
+#%练习
+print "3%4=",3%4
+print "30%4=",30%4
+
+print "Roosters",100 - 25 * 3 % 4
+
+print "Now I will count the eggd:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7"
+
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?",3 + 2
+print "What is 5 - 7?",5 - 7
+
+print "Oh,that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?",5 >= -2
+print "Is it less or equal?",5 <= -2
+
