86c4c9ef82797bfdd2187a6fb62e86a80a15ffe4
zhangting151@163.com <i151186>
2014-02-08 09:43:53 +0800

    ex13_1.py

---
+++ python/ex13_1.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+#script,first,second,third = argv
+
+print "The script is called: ",argv[0]
+print "Your first variable is: ",argv[1]
+print "Your second variable is: ",argv[2]
+print "Your third variable is: ",argv[3]
