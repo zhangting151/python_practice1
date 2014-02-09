54fb0b18c42e114e713238840f7ed9db07b240ea
zhangting151@163.com <i151186>
2014-02-09 22:20:36 +0800

    ex43.py

---
+++ python/ex43.py
@@ -0,0 +1,47 @@
+import random
+from urllib import urlopen
+import sys
+
+WORD_URL = 'http://learncodethehardway.org/words.txt'
+WORDS = []
+
+PHRASES = {
+    "class ###(###):":"Make a class named ### that is-a ###.",
+    "class ###(object):\n\tdef __init__(self,***)":"class ### has-a __init__ that takes self and *** parameters.",
+    "class ###(object):\n\tdef ***(self,@@@)":"class ### has-a function named *** that takes self and @@@ paramaters.",
+    "*** = ###()":"Set *** to an instance of class ###.",
+    "***.***(@@@)":"From *** get the *** function, and call it with paramaters self,@@@.",
+    "***.*** = '***'":"From *** get the *** attribute and set it to '***'."
+}
+
+#do they want to drill phrases first
+PHRASE_FIRST = False
+if len(sys.argv) == 2 and sys.argv[1] == "english":
+    PHRASE_FIRST = True
+
+#load up the words from the website
+for word in urlopen(WORD_URL).readlines():
+    WORD.append(word.strip())
+
+def convert(snippet,phrase):
+    class_names = [w.capitalize() for w in random.sample(WORDS,snippet.count("###"))]
+    other_names = random.sample(WORDS,snippet.count("***"))
+    results = []
+    param_names = []
+
+    for i in range(0,snippet.count("@@@")):
+        param_count = random.randint(1,3)
+        param_names.append(', '.join(random.sanple(WORDS,param_count)))
+
+    for sentence in snippet,phrase:
+        results = sentance[:]
+
+        #fake class names
+        for word in class_names:
+            result = result.replace("###",word,1)
+        #fake other names
+        for word in other_names:
+            result = result.replace("***",word,1)
+        #fake paramater lists
+        for word in param_names:
+            result = result.replace("@@@",word,1)
