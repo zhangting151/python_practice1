b01e7aa634a7e947f5df653c34448d5405a13ca8
zhangting151@163.com <i151186>
2014-02-09 20:47:23 +0800

    delete_post

---
+++ python_wordpress1/delete_post.py
@@ -0,0 +1,63 @@
+# -*- coding: cp936 -*-
+
+import unittest
+from selenium import webdriver
+import time
+
+
+class WordPressTestCase(unittest.TestCase):
+    dr = None
+    login_url = 'http://localhost/wordpress/wp-login.php'
+    post_list_url = 'http://localhost/wordpress/wp-admin/edit.php'
+
+    def setUp(self):
+        self.dr = webdriver.Firefox()
+
+    def login(self):
+        self.dr.get(self.login_url)
+        self.dr.find_element_by_name('log').send_keys('admin')
+        self.dr.find_element_by_name('pwd').send_keys('111111')
+        self.dr.find_element_by_name('wp-submit').click()
+
+    def test_delete_post(self):
+        self.login()
+        title = self.delete_post()
+        print title
+        self.dr.get(self.post_list_url)
+        post_list_table = self.dr.find_element_by_class_name('wp-list-table')
+        self.assertFalse(title in post_list_table.text)
+
+        
+    def delete_post(self):
+        post_name = self.create_post()       
+        self.dr.get('http://localhost/wordpress/wp-admin/edit.php')
+        list1 = self.dr.find_elements_by_class_name('row-title')
+        
+        for a in list1:
+            if a.text == post_name:
+                href = a.get_attribute('href') 
+                self.dr.get(href)
+                self.dr.find_element_by_class_name('submitdelete').click()
+                return post_name
+    
+    def create_post(self):
+        create_post_url = 'http://localhost/wordpress/wp-admin/post-new.php'
+        self.dr.get(create_post_url)
+        title_or_content = raw_input("please enter your post'name\n")
+        self.dr.find_element_by_name('post_title').send_keys(title_or_content)
+        js = "document.getElementById('content_ifr').contentWindow.document.body.innerHTML='" + title_or_content + "'"
+        print js
+        self.dr.execute_script(js)
+        self.dr.find_element_by_name('publish').click()
+        return title_or_content
+        
+        
+                
+                 
+                
+    def tearDown(self):
+        self.dr.quit()
+
+
+if __name__ == '__main__':
+    unittest.main()
