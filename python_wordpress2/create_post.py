159d28847454cf22bcc5a8ad4de5ecc1e381435c
zhangting151@163.com <i151186>
2014-02-09 22:18:15 +0800

    修改后版本的create_post，独立的login.py

---
+++ python_wordpress2/create_post.py
@@ -0,0 +1,41 @@
+# -*- coding: cp936 -*-
+
+import unittest
+from selenium import webdriver
+import time
+import login
+
+
+class WordPressTestCase(unittest.TestCase):
+    dr = None
+    login_url = 'http://localhost/wordpress/wp-login.php'
+    post_list_url = 'http://localhost/wordpress/wp-admin/edit.php'
+
+    def setUp(self):
+        self.dr = webdriver.Firefox()
+
+    def test_create_post(self):
+        con = login.commen(self.dr)
+        con.login()
+        title = self.create_post()
+        self.dr.get(self.post_list_url)
+        post_list_table = self.dr.find_element_by_class_name('wp-list-table')
+        self.assertTrue(title in post_list_table.text)
+
+    def create_post(self):
+        create_post_url = 'http://localhost/wordpress/wp-admin/post-new.php'
+        self.dr.get(create_post_url)
+        title_or_content = 'new post' + ' ' + str(time.time())
+        self.dr.find_element_by_name('post_title').send_keys(title_or_content)
+        js = "document.getElementById('content_ifr').contentWindow.document.body.innerHTML='" + title_or_content + "'"
+        print js
+        self.dr.execute_script(js)
+        self.dr.find_element_by_name('publish').click()
+        return title_or_content
+
+        
+    def tearDown(self):
+        self.dr.quit()
+
+if __name__ == '__main__':
+    unittest.main()
